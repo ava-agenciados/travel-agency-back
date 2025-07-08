755e155a75e9de9304f631c81627309b9df0ae71
João Gabriel <104171913+thejaumzinho@users.noreply.github.com>
2025-07-08 20:49:28 -0300

    Add: User repository and interfaces

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using travel_agency_back.Repositories.Interfaces;
+
+namespace travel_agency_back.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public void CreateUser(string firstName, string lastName, string cpfPassport, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserCpfPassportExists(string cpfPassport)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserEmailExists(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
