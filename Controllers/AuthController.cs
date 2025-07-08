36e8b211ece3f15572c2cdd401d51f9c49ead7de
João Gabriel <104171913+thejaumzinho@users.noreply.github.com>
2025-07-08 19:13:25 -0300

    Add: Auth and Package Controllers

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using travel_agency_back.DTOs;
+
+namespace travel_agency_back.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("auth/register")]
+        public Task<IActionResult> Register([FromBody]CreateUserDTO userDTO)
+        {
+            //TODO implementar a lógica de registro
+            return Task.FromResult<IActionResult>(Ok("Register successful"));
+        }
+
+
+        [HttpPost]
+        [Route("auth/login")]
+        public Task<IActionResult> Login([FromBody]LoginUserDTO userDTO)
+        {
+            // TODO implementar a lógica de autenticação
+            return Task.FromResult<IActionResult>(Ok("Login successful"));
+        }
+    }
+}
---
+++ Controllers/PackageController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using travel_agency_back.DTOs;
+
+namespace travel_agency_back.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class PackageController : ControllerBase
+    {
+        [HttpGet("packages/search")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public Task<IActionResult> PackageSearch([FromQuery] PackageSearchDTO packageSearchDTO)
+        {
+            //TODO: Retorna uma lista de todos os pacotes disponiveis
+            return Task.FromResult<IActionResult>(Ok("All packages retrieved successfully"));
+        }
+
+        [HttpGet("packages/avaliable")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public Task<IActionResult> GetAllAvaliablePackages()
+        {
+            //TODO: Retorna uma lista de todos os pacotes disponiveis
+            return Task.FromResult<IActionResult>(Ok("All packages retrieved successfully"));
+        }
+
+        [HttpGet("packages/my-packages")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public Task<IActionResult> GetMyPackages()
+        {
+            //TODO: Retorna uma lista de todos os pacotes do usuário autenticado
+            return Task.FromResult<IActionResult>(Ok("Return all user packages"));
+        }
+    }
+}
