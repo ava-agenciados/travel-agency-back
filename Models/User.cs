6ff2f277d64c947b9a1e585997bc57ac790f2a15
João Gabriel <104171913+thejaumzinho@users.noreply.github.com>
2025-07-08 20:49:01 -0300

    Add: User class

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace travel_agency_back.Models
+{
+    public class User
+    {
+        [Required]
+        private string _firstName;
+
+        [Required]
+        private string _lastName;
+
+        [Required]
+        [EmailAddress]
+        private string _email;
+
+        [Required]
+        [MinLength(6)]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, and one digit.")]
+        private string _password;
+    }
+}
